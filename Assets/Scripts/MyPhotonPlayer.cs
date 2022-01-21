3781370f782aa18dc714185e8bc7fdca0ef372e1
Bora Bilir <bora.bilir@outlook.com>
2022-01-22 01:44:05 +0300

    Player spawn points are set

--- Assets/Scripts/MyPhotonPlayer.cs
+++ Assets/Scripts/MyPhotonPlayer.cs
@@ -1,4 +1,5 @@
 using Photon.Pun;
+using Photon.Realtime;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -9,13 +10,23 @@ public class MyPhotonPlayer : MonoBehaviour
     PhotonView myPV;
     GameObject myPlayerAvatar;
 
+    Player[] allPlayers;
+    int myNumberInRoom = 0;
 
     void Start()
     {
         myPV = GetComponent<PhotonView>();
+
+        allPlayers = PhotonNetwork.PlayerList;
+        foreach (Player p in allPlayers)
+        {
+            if (p != PhotonNetwork.LocalPlayer)
+                myNumberInRoom++;
+        }
+
         if (myPV.IsMine)
         {
-            myPlayerAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "AU_Player"), Vector3.zero, Quaternion.identity);
+            myPlayerAvatar = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "AU_Player"), AU_GameController.instance.spawnPoints[myNumberInRoom].position, Quaternion.identity);
         }
     }
 
